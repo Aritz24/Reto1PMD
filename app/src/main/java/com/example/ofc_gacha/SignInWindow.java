24e32544d36fbde6ae1f146110fd729e49d7d842
Aritz <aritzerandio@gmail.com>
2022-11-05 14:21:38 +0100

    BD

--- app/src/main/java/com/example/ofc_gacha/SignInWindow.java
+++ app/src/main/java/com/example/ofc_gacha/SignInWindow.java
@@ -2,7 +2,9 @@ package com.example.ofc_gacha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,8 +21,8 @@ public class SignInWindow extends AppCompatActivity {
     private TextView pas;
     public static final int SignUpWindow= 1;
     public static final int GachaWindow= 2;
-    public static final int LibraryWindow= 3;
     private Intent intent;
+    private  SQLiteDatabase dataBase= null;
 
 
     @Override
@@ -28,6 +30,12 @@ public class SignInWindow extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_window);
 
+
+
+        dataBase= openOrCreateDatabase("Gacha", Context.MODE_PRIVATE, null);
+        dataBase.execSQL("Create TABLE IF NOT EXISTS usuarios (idUsuario INTEGER, nombre VARCHAR, contraseña VARCHAR)");
+        dataBase.execSQL("CREATE TABLE IF NOT EXISTS personajes (idPersonaje INTEGER, nombre VARCHAR, descripcion VARCHAR)");
+        dataBase.execSQL("CREATE TABLE IF NOT EXISTS biblioteca (idUsuario INTEGER, idPersonaje INTEGER)");
         signIn= findViewById(R.id.signInButton);
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,7 +70,9 @@ public class SignInWindow extends AppCompatActivity {
     }
 
     public boolean comprobarBD(){
+        if (dataBase.rawQuery("SELECT * FROM usuarios WHERE nombre="+username.getText().toString()+"").isNull()){
 
+        }
         return true;
     }
 }
